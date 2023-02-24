a07de503f62df15215e152384c14a8e4085976cf
epsilonator <somichat@gmail.com>
2023-02-24 23:59:25 +0530

    Added EC002: Continue statement should not be in a finally block

--- test.py
+++ test.py
@@ -1,26 +1,6 @@
-import abc,json
-import n1
-
-if 9:
-    import x, y
-
-import pdb
-pdb.set_trace()
-
-class X:
-    import pdb
-    pdb.set_trace()
-
-l = {1, 2, 1, 3, 4}
-l = {'PUT', 'POST', 'GET', 'POST', 'GET'}
-
-d = {
-    1: 2,
-    3: 4,
-    1: 5
-}
-if i in [0, 1]:
-    break
+try:
+    x = 1
+finally:
     continue
 
-import pdb; pdb.set_trace();
+continue
